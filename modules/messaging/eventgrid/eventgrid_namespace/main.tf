f389fa6ee1ec10e9ddd0e03db106fa0dc53a164d
Darren Hull <darren.hull>
2024-06-14 16:44:50 +0100

    added tags back into eventgrid namespace

--- modules/messaging/eventgrid/eventgrid_namespace/main.tf
+++ modules/messaging/eventgrid/eventgrid_namespace/main.tf
@@ -14,4 +14,10 @@ locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   } 
+  
+  tags = var.global_settings.inherit_tags ? merge(
+    var.global_settings.tags,
+    try(var.base_tags, null),
+    try(var.tags, null)
+  ) : try(var.tags, null)
 }

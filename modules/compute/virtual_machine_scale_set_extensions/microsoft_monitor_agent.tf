fddd713e41cb4d0b3f9fd4db38168cd774a12535
Darren Hull <darren.hull>
2024-06-21 09:33:44 +0100

    added name override for "microsoft_monitoring_agent

--- modules/compute/virtual_machine_scale_set_extensions/microsoft_monitor_agent.tf
+++ modules/compute/virtual_machine_scale_set_extensions/microsoft_monitor_agent.tf
@@ -1,3 +1,5 @@
+
+
 resource "azurerm_virtual_machine_scale_set_extension" "vmss_ext_mma" {
   for_each                     = var.extension_name == "microsoft_monitoring_agent" ? toset(["enabled"]) : toset([])
   virtual_machine_scale_set_id = var.virtual_machine_scale_set_id
@@ -5,7 +7,7 @@ resource "azurerm_virtual_machine_scale_set_extension" "vmss_ext_mma" {
   auto_upgrade_minor_version = try(var.extension.auto_upgrade_minor_version, null)
   automatic_upgrade_enabled  = try(var.extension.automatic_upgrade_enabled, null)
   force_update_tag           = try(var.extension.force_update_tag, null)
-  name                       = var.virtual_machine_scale_set_os_type == "linux" ? "OMSExtension" : "MMAExtension"
+  name                       = try(var.extension.name, var.virtual_machine_scale_set_os_type == "linux" ? "OMSExtension" : "MMAExtension")
   publisher                  = "Microsoft.EnterpriseCloud.Monitoring"
   type                       = var.virtual_machine_scale_set_os_type == "linux" ? "OmsAgentForLinux" : "MicrosoftMonitoringAgent"
   type_handler_version       = try(var.extension.type_handler_version, var.virtual_machine_scale_set_os_type == "linux" ? "1.4" : "1.0")

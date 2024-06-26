0fce9fd803e9da363ea3bb21d93335e11fca4fb5
Darren Hull <darren.hull>
2024-06-26 15:17:57 +0100

    added endpoint to eventgrid namespace topic

--- modules/messaging/eventgrid/eventgrid_namespace/output.tf
+++ modules/messaging/eventgrid/eventgrid_namespace/output.tf
@@ -6,3 +6,8 @@ output "identity" {
   value       = azapi_resource.egn.identity
   description = "An `identity` block as defined below, which contains the Managed Service Identity information for this Event Grid Namespace."
 }
+
+output "endpoint" {
+  value       = azapi_resource.egn.properties["endpoint"]
+  description = "The endpoint of the Event Grid Namespace."
+}

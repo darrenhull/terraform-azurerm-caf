f634cefcf9848174b03e73ab9721a4cc0e1adaf1
Darren Hull <darren.hull>
2024-07-04 14:49:09 +0100

    fixed app container keyvault secret values

--- modules/compute/container_app/keyvault_secrets.tf
+++ modules/compute/container_app/keyvault_secrets.tf
@@ -2,11 +2,11 @@
 data "azurerm_key_vault_secret" "secret" {
   for_each = {
     for key, value in try(var.settings.secret, {}) : key => value
-    if try(s.keyvault, null) != null
+    if try(value.keyvault, null) != null
   }
-  name     = var.settings.secret.keyvault.secret_name
+  name     = each.value.keyvault.secret_name
   key_vault_id = try(
-    var.settings.secret.keyvault.id,
-    var.combined_resources.keyvaults[try(var.settings.secret.keyvault.lz_key, var.client_config.landingzone_key)][var.settings.secret.keyvault.key].id,
+    each.value.keyvault.id,
+    var.combined_resources.keyvaults[try(each.value.keyvault.lz_key, var.client_config.landingzone_key)][each.value.keyvault.key].id,
   )
 }

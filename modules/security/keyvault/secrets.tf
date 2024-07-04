9e4274ec483bbb159efedd35cddd3106796d7bda
Darren Hull <darren.hull>
2024-07-04 16:44:22 +0100

    added depends on role assignment to keyvault secret

--- modules/security/keyvault/secrets.tf
+++ modules/security/keyvault/secrets.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_key_vault_secret" "secret" {
-  depends_on = [module.initial_policy]
+  depends_on = [module.initial_policy,azurerm_role_assignment.for]
   for_each = {
     for key, value in try(var.settings.secrets, {}) : key => value
     if try(value.ignore_changes, false) == false
@@ -17,7 +17,7 @@ resource "azurerm_key_vault_secret" "secret" {
 
 # workaround until support for https://github.com/hashicorp/terraform/issues/25534
 resource "azurerm_key_vault_secret" "secret_ignore_changes" {
-  depends_on = [module.initial_policy]
+  depends_on = [module.initial_policy,azurerm_role_assignment.for]
   for_each = {
     for key, value in try(var.settings.secrets, {}) : key => value
     if try(value.ignore_changes, false) == true

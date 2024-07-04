34e1a52006c009e5532e935b0dd290edd0bd2fc9
Darren Hull <darren.hull>
2024-07-04 13:11:05 +0100

    fix container app key vault setting.

--- modules/compute/container_app/keyvault_secrets.tf
+++ modules/compute/container_app/keyvault_secrets.tf
@@ -1,6 +1,6 @@
 
 data "azurerm_key_vault_secret" "secret" {
-  for_each = { for s in var.settings.secret : s.keyvault => s if s.keyvault != null }
+  for_each = { for s in try(var.settings.secret, {}) : s.key => s if try(s.keyvault, null) != null }
   name     = var.settings.secret.keyvault.secret_name
   key_vault_id = try(
     var.settings.secret.keyvault.id,

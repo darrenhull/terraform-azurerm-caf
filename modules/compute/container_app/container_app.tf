719e09a017bc8fd97734277c0ee26a9f28bb44ec
Darren Hull <darren.hull>
2024-07-04 21:05:43 +0100

    added managed identity to secret

--- modules/compute/container_app/container_app.tf
+++ modules/compute/container_app/container_app.tf
@@ -257,6 +257,7 @@ resource "azurerm_container_app" "ca" {
       name  = secret.value.name
       value = try(secret.value.value, null)
       key_vault_secret_id = try(data.azurerm_key_vault_secret.secret[secret.name].resource_versionless_id, null)
+      identity = can(secret.value.identity.managed_identity_key)? var.combined_resources.managed_identities[try(secret.value.identity.lz_key, var.client_config.landingzone_key)][secret.value.identity.managed_identity_key].id : "System"
     }
   }
 

1d35b33fe103c21efc64eb4069f7e0dfbae25981
Darren Hull <darren.hull>
2024-07-04 16:41:20 +0100

    added depends_on role mapping to container app

--- container_apps.tf
+++ container_apps.tf
@@ -1,6 +1,7 @@
 module "container_apps" {
   source   = "./modules/compute/container_app"
   for_each = local.compute.container_apps
+  depends_on = [azurerm_role_assignment.for]
 
   location                                 = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group                           = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
